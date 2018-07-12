1ecb35d3f2fc4b9598b97a9505ba0210a3834220
Toshiba <Toshiba@192.168.1.7>
2018-07-12 21:12:19 +0200

    Game GUI extension

--- src/pl/chizynski/kolkokrzyzyk/Window.java
+++ src/pl/chizynski/kolkokrzyzyk/Window.java
@@ -1,13 +1,8 @@
 package pl.chizynski.kolkokrzyzyk;
 
-import java.awt.Dimension;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import pl.chizynski.kolkokrzyzyk.boards.Board;
 
@@ -24,135 +19,58 @@ public class Window extends JFrame {
         compsToExperiment.setLayout(new GridLayout(3,3));
         JPanel controls = new JPanel(); 
         JButton button1 = new JButton("Button 1");
-        
-        
-        button1.addActionListener(new ActionListener(){ //klasa anomimowa,
-            //jesli interfejs ma 1 metode to interfejs funkcyjny ze mozna go uzywac w programowaniu funkcyjnym
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("test");
-            }
-        });
+        JButton button2 = new JButton("Button 2");
+        JButton button3 = new JButton("Button 3");
+        JButton button4 = new JButton("Button 4");
+        JButton button5 = new JButton("Button 5");
+        JButton button6 = new JButton("Button 6");
+        JButton button7 = new JButton("Button 7");
+        JButton button8 = new JButton("Button 8");
+        JButton button9 = new JButton("Button 9");
+//        button1.addActionListener(new ActionListener(){ //klasa anomimowa,
+//            //jesli interfejs ma 1 metode to interfejs funkcyjny ze mozna go uzywac w programowaniu funkcyjnym
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                System.out.println("test");
+//            }
+//        });
         //Add buttons to experiment with Grid Layout
-        button1.addActionListener((e)->{
+        button1.addActionListener((e)->{     //lambda funkcja anonimowa do interfejsu funkcyjnego
             this.game.getBoard().getFields()[0][0] = Board.CROSS;
             this.game.getBoard().displayState();
         }); 
-       
-        JButton button2 = new JButton("Button 2");
-         button2.addActionListener(new ActionListener(){ //klasa anomimowa,
-            //jesli interfejs ma 1 metode to interfejs funkcyjny ze mozna go uzywac w programowaniu funkcyjnym
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("test");
-            }
-        });
-        //Add buttons to experiment with Grid Layout
         button2.addActionListener((e)->{
             this.game.getBoard().getFields()[0][1] = Board.CROSS;
             this.game.getBoard().displayState();
         }); 
-        
-        JButton button3 = new JButton("Button 3");
-         button3.addActionListener(new ActionListener(){ //klasa anomimowa,
-            //jesli interfejs ma 1 metode to interfejs funkcyjny ze mozna go uzywac w programowaniu funkcyjnym
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("test");
-            }
-        });
-        //Add buttons to experiment with Grid Layout
         button3.addActionListener((e)->{
             this.game.getBoard().getFields()[0][2] = Board.CROSS;
             this.game.getBoard().displayState();
         }); 
-        
-        JButton button4 = new JButton("Button 4");
-         button4.addActionListener(new ActionListener(){ //klasa anomimowa,
-            //jesli interfejs ma 1 metode to interfejs funkcyjny ze mozna go uzywac w programowaniu funkcyjnym
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("test");
-            }
-        });
-        //Add buttons to experiment with Grid Layout
         button4.addActionListener((e)->{
             this.game.getBoard().getFields()[1][0] = Board.CROSS;
             this.game.getBoard().displayState();
-        }); 
-        
-        JButton button5 = new JButton("Button 5");
-         button5.addActionListener(new ActionListener(){ //klasa anomimowa,
-            //jesli interfejs ma 1 metode to interfejs funkcyjny ze mozna go uzywac w programowaniu funkcyjnym
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("test");
-            }
         });
-        //Add buttons to experiment with Grid Layout
         button5.addActionListener((e)->{
             this.game.getBoard().getFields()[1][1] = Board.CROSS;
             this.game.getBoard().displayState();
-        }); 
-        
-        JButton button6 = new JButton("Button 6");
-         button6.addActionListener(new ActionListener(){ //klasa anomimowa,
-            //jesli interfejs ma 1 metode to interfejs funkcyjny ze mozna go uzywac w programowaniu funkcyjnym
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("test");
-            }
         });
-        //Add buttons to experiment with Grid Layout
         button6.addActionListener((e)->{
             this.game.getBoard().getFields()[1][2] = Board.CROSS;
             this.game.getBoard().displayState();
         }); 
-        
-        JButton button7 = new JButton("Button 7");
-         button7.addActionListener(new ActionListener(){ //klasa anomimowa,
-            //jesli interfejs ma 1 metode to interfejs funkcyjny ze mozna go uzywac w programowaniu funkcyjnym
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("test");
-            }
-        });
-        //Add buttons to experiment with Grid Layout
         button7.addActionListener((e)->{
             this.game.getBoard().getFields()[2][0] = Board.CROSS;
             this.game.getBoard().displayState();
         }); 
-        
-        JButton button8 = new JButton("Button 8");
-         button8.addActionListener(new ActionListener(){ //klasa anomimowa,
-            //jesli interfejs ma 1 metode to interfejs funkcyjny ze mozna go uzywac w programowaniu funkcyjnym
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("test");
-            }
-        });
-        //Add buttons to experiment with Grid Layout
         button8.addActionListener((e)->{
             this.game.getBoard().getFields()[2][1] = Board.CROSS;
             this.game.getBoard().displayState();
         }); 
-        JButton button9 = new JButton("Button 9");
-         button9.addActionListener(new ActionListener(){ //klasa anomimowa,
-            //jesli interfejs ma 1 metode to interfejs funkcyjny ze mozna go uzywac w programowaniu funkcyjnym
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("test");
-            }
-        });
-        //Add buttons to experiment with Grid Layout
-        button9.addActionListener((e)->{
+        button9.addActionListener((e)->{ 
             this.game.getBoard().getFields()[2][2] = Board.CROSS;
             this.game.getBoard().displayState();
-        }); 
-        
-        
-        
-//lambda funkcja anonimowa do interfejsu funkcyjnego
+        });
         compsToExperiment.add(button1);
         compsToExperiment.add(button2);
         compsToExperiment.add(button3);
@@ -162,10 +80,8 @@ public class Window extends JFrame {
         compsToExperiment.add(button7);
         compsToExperiment.add(button8);
         compsToExperiment.add(button9);
-
         frame.getContentPane().add(compsToExperiment);
         frame.setVisible(true);// pokazanie okna 
         frame.pack(); //spakowanie okna 
-
     }
 }
